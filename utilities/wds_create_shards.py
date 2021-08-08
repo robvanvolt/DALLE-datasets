635830a1693ba369d01d82cc6d80bf1b517eeb29
robvanvolt <65366998+robvanvolt@users.noreply.github.com>
2021-08-08 19:54:36 +0200

    Update wds_create_shards.py
    Updated recursive search.

--- utilities/wds_create_shards.py
+++ utilities/wds_create_shards.py
@@ -61,13 +61,13 @@ def readfile(fname):
         return stream.read()
 
 path = Path(args.data)
-text_files = [*path.glob('**/*.txt')]
+text_files = [*path.glob('**/*.txt', recursive=True)]
 text_files = {text_file.stem: text_file for text_file in text_files} # str(text_file.parents[0]) + 
 text_total = len(text_files)
 
 image_files = [
-    *path.glob('**/*.png'), *path.glob('**/*.jpg'),
-    *path.glob('**/*.jpeg'), *path.glob('**/*.bmp')
+    *path.glob('**/*.png', recursive=True), *path.glob('**/*.jpg', recursive=True),
+    *path.glob('**/*.jpeg', recursive=True), *path.glob('**/*.bmp', recursive=True)
 ]
 image_files = {image_file.stem: image_file for image_file in image_files} # str(image_file.parents[0]) +
 image_total = len(image_files)
