eb8aefe623f98f92b95c57d401a8f8d264d51e78
robvanvolt <robvanvolt@gmail.com>
2021-09-22 12:46:20 +0200

    Fixed cc3m and cc12m script

--- general/cc12m.py
+++ general/cc12m.py
@@ -8,68 +8,41 @@ from multiprocessing import Pool
 import gc
 import glob
 
-
-# https://github.com/google-research-datasets/conceptual-12m
 cc_url = 'https://storage.googleapis.com/conceptual_12m/cc12m.tsv'
-
-root_folder = ''
-
-if not os.path.isfile(root_folder + '/cc12m.tsv'):
-    print('Missing cc12m url-caption-dataset. Downloading...')
-else:
-    print('cc12m.tsv already downloaded. Proceeding with downloading images!')
-
-dfc = pd.read_csv(root_folder + "/cc12m.tsv", sep='\t', names=["url", "caption"])
-image_folder = root_folder + '/images'
-text_folder = root_folder + '/texts'
-output_folder = root_folder + '/output'
-skip_folder = root_folder + '/skip'
-paths = [image_folder, text_folder, output_folder, skip_folder]
-for path in paths:
-    os.makedirs(path, exist_ok=True)
+root_folder = './'
 total = 12423374
 maxwidth = 256
 maxheight = 256
-thread_count = 64
-
+thread_count = 16
+batch = 10000
 
 def load_caption(x):
-    name, caption = x
-    subdir = str(int(int(name) / 10000 )) 
-    if not os.path.exists(text_folder+"/"+subdir):
-        try:
-            os.makedirs(text_folder+"/"+subdir)
-        except:
-            pass
-    id = subdir + "/" + "0"*(9-len(str(name))) + str(name)
-    with open(Path(text_folder + '/' + id + '.txt'), 'w') as f:
+    name, caption, text_folder = x
+    fid = str(int(int(name) / 10000 )) 
+    subdir = "0"*(5-len(fid)) + fid
+    os.makedirs(Path(text_folder+"/"+subdir), exist_ok=True)
+    fp = text_folder + '/' + subdir + "/" + "0"*(9-len(str(name))) + str(name) + '.txt'
+    with open(fp, 'w') as f:
         f.write(caption)
 
-def list_ids(path):
-    return [int(os.path.splitext(os.path.basename(a))[0]) for a in glob.glob(path+"/**/*")]
-
-skiplist = list_ids(text_folder)
-remaining = total - len(skiplist)
-percent_remaining = 100 * (total - remaining) / total
-df = dfc.loc[~dfc.index.isin(skiplist)]
-print('Remaining {} captions to be written - {} ({:.5f} %) already written.'.format(remaining, len(skiplist), percent_remaining))
-if len(df) > 0:
-    captions = zip(df.index, df["caption"])
-    pool = Pool(16)
-    for _ in tqdm(pool.imap_unordered(load_caption, captions), total=len(df)):
-            pass
-    pool.close()
-print('Done with captions!')
-
+def download_file(url):
+    response = requests.get(url, stream=True)
+    total_size_in_bytes= int(response.headers.get('content-length', 0))
+    block_size = 1024
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    with open(Path(root_folder + '/cc12m.tsv'), 'wb') as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("Error, something went wrong...")
 
 def load_image(x):
-    name, url = x
-    subdir = str(int(int(name) / 10000 )) 
-    if not os.path.exists(image_folder+"/"+subdir):
-        try:
-            os.makedirs(image_folder+"/"+subdir)
-        except:
-            pass
+    name, url, image_folder, skip_folder = x
+    fid = str(int(int(name) / 10000 )) 
+    subdir = "0"*(5-len(fid)) + fid
+    os.makedirs(Path(image_folder+"/"+subdir), exist_ok=True)
     id = subdir + "/" + "0"*(9-len(str(name))) + str(name)
     try:
         with Image.open(requests.get(url,
@@ -80,31 +53,61 @@ def load_image(x):
             with open(Path(image_folder + "/" + id + '.jpg'), 'wb') as file:
                 foo.save(file, optimize=True, quality=85)
     except Exception:
-        if not os.path.exists(skip_folder+"/"+subdir):
-            try:
-                os.makedirs(skip_folder+"/"+subdir)
-            except:
-                pass
+        os.makedirs(Path(skip_folder+"/"+subdir), exist_ok=True)
         open(Path(skip_folder + '/' + id), 'a').close
         pass
 
-skiplist = list_ids(skip_folder) + list_ids(image_folder)
-remaining = total - len(skiplist)
-percent_remaining = 100 * (total - remaining) / total
+if __name__ == '__main__':
+    if not os.path.isfile(Path(root_folder + '/cc12m.tsv')):
+        print('Missing cc12m url-caption-dataset. Downloading...')
+        download_file(cc_url)
+    else:
+        print('cc12m.tsv already downloaded. Proceeding with downloading images!')
 
-df = dfc.loc[~dfc.index.isin(skiplist)]
-print('Remaining {} images to be downloaded - {} ({:.5f} %) already downloaded.'.format(remaining, len(skiplist), percent_remaining))
-images = list(zip(df.index, df["url"]))
+    dfc = pd.read_csv(root_folder + "cc12m.tsv", sep='\t', names=["url", "caption"])
 
-batch = 20000
+    image_folder = root_folder + '/images'
+    text_folder = root_folder + '/texts'
+    skip_folder = root_folder + '/skip'
 
-for i in tqdm(range(0, len(df), batch)):
-    pool = Pool(thread_count)
-    for _ in tqdm(pool.imap_unordered(load_image, images[i:i+batch]), total=batch):
-        pass
-    pool.terminate()
-    pool.join()
-    del pool
-    gc.collect()
+    paths = [image_folder, text_folder, skip_folder]
 
-print('Finished downloading available images from conceptual images!')
+    for path in paths:
+        os.makedirs(path, exist_ok=True)
+
+    def list_ids(path):
+        return [int(os.path.splitext(os.path.basename(a))[0]) for a in glob.glob(path+"/**/*")]
+
+    skiplist = list_ids(text_folder)
+    remaining = total - len(skiplist)
+    percent_remaining = 100 * (total - remaining) / total
+    df = dfc.loc[~dfc.index.isin(skiplist)]
+
+    print('Remaining {} captions to be written - {} ({:.5f} %) already written.'.format(remaining, len(skiplist), percent_remaining))
+
+    if len(df) > 0:
+        captions = zip(df.index, df["caption"], [text_folder]*len(df))
+        pool = Pool(thread_count)
+        for _ in tqdm(pool.imap_unordered(load_caption, captions), total=len(df)):
+                pass
+        pool.close()
+    print('Done with captions!')
+
+    skiplist = list_ids(skip_folder) + list_ids(image_folder)
+    remaining = total - len(skiplist)
+    percent_remaining = 100 * (total - remaining) / total
+
+    df = dfc.loc[~dfc.index.isin(skiplist)]
+    print('Remaining {} images to be downloaded - {} ({:.5f} %) already downloaded.'.format(remaining, len(skiplist), percent_remaining))
+    images = list(zip(df.index, df["url"], [image_folder]*len(df), [skip_folder]*len(df)))
+
+    for i in tqdm(range(0, len(df), batch)):
+        pool = Pool(thread_count)
+        for _ in tqdm(pool.imap_unordered(load_image, images[i:i+batch]), total=batch):
+            pass
+        pool.terminate()
+        pool.join()
+        del pool
+        gc.collect()
+
+    print('Finished downloading available images from conceptual images!')
--- general/cc3m.py
+++ general/cc3m.py
@@ -1,19 +1,21 @@
 import pandas as pd
 from pathlib import Path
 from PIL import Image
+from tqdm import tqdm
 import requests
-import zipfile
 import os
 from pandarallel import pandarallel
+
+### seperator = |
 
 ##### https://ai.google.com/research/ConceptualCaptions/download
 ##### download url-caption dataset from 
 ##### https://storage.cloud.google.com/gcc-data/Train/GCC-training.tsv?_ga=2.191230122.-1896153081.1529438250
 
-DATASETFOLDER = '.'
-DATASETZIP = 'yfcc_filtered.zip'
+DATASETFOLDER = 'content'
 DATASET = 'Train_GCC-training.tsv'
 FILEID = '1edNr-GEYz69RWcsSgskNzjtM--Qxepdz'
+URL = 'https://storage.cloud.google.com/gcc-data/Train/GCC-training.tsv?_ga=2.191230122.-1896153081.1529438250'
 
 ##### download location of image-caption pairs
 PARENTPATH = 'output'
@@ -27,7 +29,7 @@ IMAGEFORMATS = ['jpg', 'jpeg', 'bmp', 'png']
 MAXWIDTH = 320
 MAXHEIGHT = 320
 CHUNKS = 500000
-THREAD_COUNT = 128
+THREAD_COUNT = 16
 HIDE_ERRORS = False
 
 os.makedirs(Path(DATASETFOLDER), exist_ok=True)
@@ -44,6 +46,19 @@ def download_file_from_google_drive(id, destination):
 
     save_response_content(response, destination)
 
+def download_file(url, root_folder):
+    response = requests.get(url, stream=True)
+    total_size_in_bytes= int(response.headers.get('content-length', 0))
+    block_size = 1024
+    progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+    with open(Path(root_folder + '/cc3.tsv'), 'wb') as file:
+        for data in response.iter_content(block_size):
+            progress_bar.update(len(data))
+            file.write(data)
+    progress_bar.close()
+    if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+        print("Error, something went wrong...")
+
 def get_confirm_token(response):
     for key, value in response.cookies.items():
         if key.startswith('download_warning'):
@@ -57,65 +72,65 @@ def save_response_content(response, destination):
             if chunk: # filter out keep-alive new chunks
                 f.write(chunk)
 
-if not os.path.isfile(Path(DATASETFOLDER + '/' + DATASET)):
-    if not os.path.isfile(Path(DATASETFOLDER + '/' + DATASETZIP)):
-        download_file_from_google_drive(FILEID, Path(DATASETFOLDER + '/' + DATASETZIP))
+if __name__ == '__main__':
+    assert os.path.isfile(Path(DATASETFOLDER + '/' + DATASET)), print('''
+        #################################################################################################################
+        Missing cc3m url-caption-dataset. Automatic downloading not supported yet.
+        Download https://storage.cloud.google.com/gcc-data/Train/GCC-training.tsv?_ga=2.191230122.-1896153081.1529438250
+        And put it into following folder: {}
+        #################################################################################################################
+        '''.format(DATASETFOLDER))
 
-    with zipfile.ZipFile(Path(DATASETFOLDER + '/' + DATASETZIP), 'r') as zip_ref:
-        zipname = zip_ref.namelist()[0].split('/')[-1]
+    pandarallel.initialize(nb_workers=THREAD_COUNT)
 
-    with zipfile.ZipFile(Path(DATASETFOLDER + '/' + DATASETZIP), 'r') as zip_ref:
-        zip_ref.extractall()
-        os.rename(Path(DATASETFOLDER + '/' + zipname), Path(DATASETFOLDER + '/' + DATASET))
+    ### downloading dataset and resizsing images in parallel
+    def write_files(x, folderpath):
+        id = PREFIX + "0"*(8-len(str(x.name))) + str(x.name)
+        try:
+            foo = Image.open(requests.get(x.url, stream=True, timeout=4).raw)
+            a = max(MAXWIDTH/foo.size[0], MAXHEIGHT/foo.size[1])
+            foo = foo.resize((int(foo.size[0] * a), int(foo.size[1] * a)), Image.ANTIALIAS)
+            foo.save(Path(folderpath + '/' + id + '.jpg'), optimize=True, quality=85)
+        except Exception as exc:
+            if not HIDE_ERRORS:
+                print('Failed downloading {} with url {}'.format(id, x.url))
+                print(exc)
+            pass
+        else:
+            with open(Path(folderpath + '/' + id + '.txt'), 'w') as f:
+                f.write(x.caption)
 
-pandarallel.initialize(nb_workers=THREAD_COUNT)
+    os.makedirs(Path(PARENTPATH), exist_ok=True)
 
-### downloading dataset and resizsing images in parallel
-def write_files(x, folderpath):
-    id = PREFIX + "0"*(8-len(str(x.name))) + str(x.name)
-    try:
-        foo = Image.open(requests.get(x.url, stream=True, timeout=4).raw)
-        a = max(MAXWIDTH/foo.size[0], MAXHEIGHT/foo.size[1])
-        foo = foo.resize((int(foo.size[0] * a), int(foo.size[1] * a)), Image.ANTIALIAS)
-        foo.save(Path(folderpath + '/' + id + '.jpg'), optimize=True, quality=85)
-    except Exception as exc:
-        if not HIDE_ERRORS:
-            print('Failed downloading {} with url {}'.format(id, x.url))
-            print(exc)
-        pass
+    keep_downloading = True
+    if CHECKALLFOLDERS:
+        batch = 0
     else:
-        with open(Path(folderpath + '/' + id + '.txt'), 'w') as f:
-            f.write(x.caption)
+        batch = len(os.listdir(Path(PARENTPATH))) - 1
+        batch = 0 if batch == -1 else batch
 
-os.makedirs(Path(PARENTPATH), exist_ok=True)
+    while keep_downloading:
+        try:
+            df = pd.read_csv(Path(DATASETFOLDER + '/' + DATASET), sep="\t", skiprows=range(0, batch * CHUNKS), nrows=CHUNKS, names=KEEPTHESECOLS)
+            # df = pd.read_csv(Path(DATASETFOLDER + '/' + DATASET), sep="\t", skiprows=range(0, batch * CHUNKS), nrows=CHUNKS, names=KEEPTHESECOLS)
+            df.index = [x + batch * CHUNKS for x in list(df.index)]
+            folderid = str(PREFIX) + "0"*(4-len(str(batch))) + str(batch)
+            folderpath = PARENTPATH + '/' + folderid
+            os.makedirs(folderpath, exist_ok=True)
+            skip = list(set([int(x[1:-4]) for x in os.listdir(folderpath)]))
+            df = df[~df.index.isin(skip)]
+            print('Saving {} images to {}.'.format(len(df), folderpath))
+            print('Skipping {} already downloaded urls.'.format(len(skip)))
+            df.apply(lambda x: write_files(x, folderpath), axis=1)
+            # df.parallel_apply(lambda x: write_files(x, folderpath), axis=1)
+        except Exception as excp:
+            print('An error occurred trying to download the filtered dataframe.')
+            print(excp)
+            keep_downloading = False
+            pass
+        else:
+            if len(df) == 0:
+                print('Alredy finished downloading images of batch {}!'.format(batch))
+            batch += 1
 
-keep_downloading = True
-if CHECKALLFOLDERS:
-    batch = 0
-else:
-    batch = len(os.listdir(Path(PARENTPATH))) - 1
-    batch = 0 if batch == -1 else batch
-
-while keep_downloading:
-    try:
-        df = pd.read_csv(Path(DATASETFOLDER + '/' + DATASET), sep="\t", skiprows=range(0, batch * CHUNKS), nrows=CHUNKS, names=KEEPTHESECOLS)
-        df.index = [x + batch * CHUNKS for x in list(df.index)]
-        folderid = PREFIX + "0"*(4-len(str(batch))) + str(batch)
-        folderpath = PARENTPATH + '/' + folderid
-        os.makedirs(folderpath, exist_ok=True)
-        skip = list(set([int(x[1:-4]) for x in os.listdir(folderpath)]))
-        df = df[~df.index.isin(skip)]
-        print('Saving {} images to {}.'.format(len(df), folderpath))
-        print('Skipping {} already downloaded urls.'.format(len(skip)))
-        df.parallel_apply(lambda x: write_files(x, folderpath), axis=1)
-    except Exception as excp:
-        print('An error occurred trying to download the filtered dataframe.')
-        print(excp)
-        keep_downloading = False
-        pass
-    else:
-        if len(df) == 0:
-            print('Alredy finished downloading images of batch {}!'.format(batch))
-        batch += 1
-
-print('Finished downloading dataset to {}.'.format(PARENTPATH))
+    print('Finished downloading dataset to {}.'.format(PARENTPATH))
